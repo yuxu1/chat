d62c2f3aa88958964dd3220349fdf2ba0c278e75
Yufan <yuxu1@users.noreply.github.com>
2024-03-03 18:32:45 -0800

    installed Gifted Chat & create basic layout for Chat screen

--- components/Chat.js
+++ components/Chat.js
@@ -1,26 +1,91 @@
-import { StyleSheet, View, Text, Button } from 'react-native';
-import { useEffect } from 'react';
+import { StyleSheet, View, Text, KeyboardAvoidingView, Platform} from 'react-native';
+import { useEffect, useState } from 'react';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 const Chat = ({ route, navigation }) => {
+  //extract user-inputted name and selected background color from parameters (from Start screen)
   const { name, bgColor } = route.params;
+
+  const [messages, setMessages] = useState([]);
 
   //set navigation header's title to the name passed from Start screen
   useEffect(() => {
     navigation.setOptions({title: name});
   }, []);
 
+  //set messages state with 2 static messages (a system message & a user message)
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 2,
+        text: 'Welcome!',
+        createdAt: new Date(),
+        user: {
+         _id: 2,
+         name: 'React Native',
+         avatar: 'https://placeimg.com/140/140/any'
+        }
+      },
+      {
+        _id: 1,
+        text: `${name} has entered the chat`,
+        createdAt: new Date(),
+        system: true
+      }
+    ]);
+  }, []);
+
+  /*
+  custom function called when a user sends a message;
+  appends the new message to the conversation (using the latest value of the messages state)
+  */
+  const onSend = (newMessages) => {
+    setMessages (previousMessages => GiftedChat.append(previousMessages, newMessages))
+  };
+
+  //function return an altered version of Gifted Chat's speech bubble (customization)
+  const renderBubble = (props) => {
+    return <Bubble
+      //inherit props
+      {...props}
+      wrapperStyle = {{
+        //target right (messages sent from user) & left (sent from others) speech bubbles
+        right: {
+          backgroundColor: '#000'
+        },
+        left: {
+          backgroundColor: '#FFF'
+        }
+      }}
+    />
+  };
+
+  //return chat interface using Gifted Chat's own GiftedChat component
   return (
     <View style={[styles.container, {backgroundColor: bgColor}]}>
-      <Text>Chat Screen</Text>
+      <GiftedChat styles={styles.chatInterface}
+        messages = {messages}
+        renderBubble = {renderBubble}
+        onSend = {messages => onSend(messages)}
+        user = {{
+          _id: 1
+        }}
+      />
+      {/*
+        fix Android issue of keyboard hiding message input field;
+        add KeyboardAvoidingView component only if the user's platform OS is Android
+      */}
+      { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    flex: 1
+  },
+  chatInterface: {
+    color: '#000'
   }
 });
 
--- components/Start.js
+++ components/Start.js
@@ -1,113 +1,149 @@
-import { StyleSheet, View, Text, TouchableOpacity, TextInput, ImageBackground } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform
+} from 'react-native';
+
 import { useState } from 'react';
 
 const Start = ({ navigation }) => {
   const [name, setName] = useState('');
   const [bgColor, setBgColor] = useState('');
-  const colors = ['#090C08','#474056','#8A95A5','#B9C6AE'];
+  const colors = ['#090C08', '#474056', '#8A95A5', '#B9C6AE'];
 
   return (
     //background image
-    <ImageBackground source={require('../assets/background-image.png')} style={styles.container}>
+    <ImageBackground
+      source={require('../assets/background-image.png')}
+      style={styles.container}
+    >
       {/* app title */}
       <Text style={styles.titleText}>CHAT</Text>
       {/* box for user name input and background color selection */}
+
+    {/* user interaction section */}
       <View style={styles.box}>
         {/* user name input (to be displayed when enter chatroom) */}
         <TextInput
           style={styles.textInput}
           value={name}
           onChangeText={setName}
-          placeholder='Your Name'
+          placeholder="Your Name"
         />
-        <Text style={styles.chooseBackgroundColor}>Choose Background Color:</Text>
+
+        <Text style={styles.chooseBackgroundColor}>
+          Choose Background Color:
+        </Text>
         {/* buttons for background color options that user can select
         (sets selected color as background color of chatroom) */}
         <View style={styles.backgroundColorOptions}>
-          {colors.map((color,index) => (
+          {colors.map((color, index) => (
             <TouchableOpacity
               key={index}
               /*set each button's color to corresponding color in colors array
               change styling to reflect selection status if button color matches bgColor(when user presses it)*/
-              style={[styles.colorButton, {backgroundColor: color}, bgColor === color && styles.selectedColor]}
+              style={[
+                styles.colorButton,
+                { backgroundColor: color },
+                bgColor === color && styles.selectedColor,
+              ]}
               onPress={() => setBgColor(color)}
             />
           ))}
         </View>
+        
         {/* button to navigate to Chat screen */}
-        <TouchableOpacity style={styles.startChatButton} onPress={() => navigation.navigate('Chat', {name: name, bgColor: bgColor})}>
-         <Text style={styles.buttonInnerText}>Start Chatting</Text>
+        <TouchableOpacity
+          style={styles.startChatButton}
+          onPress={() =>
+            navigation.navigate('Chat', { name: name, bgColor: bgColor })
+          }
+        >
+          <Text style={styles.buttonInnerText}>Start Chatting</Text>
         </TouchableOpacity>
+
+        {/* 
+          fix keyboard issue on iOS - keyboard covers name & background color picker form;
+          adds KeyBoardAvoidingView component only if user's platform OS is iOS
+         */}
+        {Platform.OS === 'ios' ? (
+          <KeyboardAvoidingView behavior="padding" />
+        ) : null}
       </View>
     </ImageBackground>
   );
-}
+};
 
 const styles = StyleSheet.create({
- container: {
-   flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center'
- },
- titleText: {
-  flex: 1,
-  fontSize: 45,
-  fontWeight: '600',
-  color: '#ffffff',
-  marginTop: 80
- },
- box: {
-  flex: 1,
-  width: '88%',
-  height: '44%',
-  marginBottom: 20, 
-  backgroundColor: '#ffffff',
-  justifyContent: 'space-evenly',
-  alignItems: 'center'
- },
- textInput: {
-  width: '88%',
-  fontSize: 16,
-  fontWeight: '300',
-  color: '#757083',
-  opacity: 0.5,
-  padding: 15,
-  borderWidth: 1,
-  marginTop: 15,
-  marginBottom: 15
- },
- chooseBackgroundColor: {
-  fontSize: 16,
-  fontWeight: '300',
-  color: '#757083',
-  opacity: 1
- },
- backgroundColorOptions: {
-  flexDirection: 'row',
-  width: '88%',
-  justifyContent: 'space-between'
- },
- colorButton: {
-  width: 40,
-  height: 40,
-  borderRadius: 20,
- },
- selectedColor: {
-  borderColor: '#757083',
-  borderWidth: 1,
-  borderLeftWidth: 4
- },
- startChatButton: {
-  width: '88%',
-  backgroundColor: '#757083',
- },
- buttonInnerText: {
-  color: '#ffffff',
-  fontSize: 16,
-  fontWeight: '600',
-  textAlign: 'center',
-  margin: 15,
- }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titleText: {
+    flex: 1,
+    fontSize: 45,
+    fontWeight: '600',
+    color: '#ffffff',
+    marginTop: 80,
+  },
+  box: {
+    flex: 1,
+    width: '88%',
+    height: '44%',
+    marginBottom: 20,
+    backgroundColor: '#ffffff',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  textInput: {
+    width: '88%',
+    fontSize: 16,
+    fontWeight: '300',
+    color: '#757083',
+    opacity: 0.5,
+    padding: 15,
+    borderWidth: 1,
+    marginTop: 15,
+    marginBottom: 15,
+  },
+  chooseBackgroundColor: {
+    fontSize: 16,
+    fontWeight: '300',
+    color: '#757083',
+    opacity: 1,
+  },
+  backgroundColorOptions: {
+    flexDirection: 'row',
+    width: '88%',
+    justifyContent: 'space-between',
+  },
+  colorButton: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+  selectedColor: {
+    borderColor: '#757083',
+    borderWidth: 1,
+    borderLeftWidth: 4,
+  },
+  startChatButton: {
+    width: '88%',
+    backgroundColor: '#757083',
+  },
+  buttonInnerText: {
+    color: '#ffffff',
+    fontSize: 16,
+    fontWeight: '600',
+    textAlign: 'center',
+    margin: 15,
+  },
 });
 
 export default Start;
